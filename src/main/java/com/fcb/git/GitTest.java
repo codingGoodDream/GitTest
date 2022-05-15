ec7175dd691d85b9cf715ead338d19d5567d1b89
fcb <2835757846@qq.com>
2022-05-15 18:47:42 +0800

    first commit

---
+++ src/main/java/com/fcb/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.fcb.git;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git");
+		System.out.println("hello git 2");
+	}
+
+}
